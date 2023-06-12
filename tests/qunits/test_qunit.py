26cd66f90512f50af04d0147390e3a0036a2089d
Davide Lanza <lanza.davide.it@gmail.com>
2023-06-12 18:36:39 +0200

    Added SensorialUnit to qunits tests

--- tests/qunits/test_qunit.py
+++ tests/qunits/test_qunit.py
@@ -7,7 +7,7 @@ import pytest_check as check
 import qrobot
 from qrobot.bursts import ZeroBurst
 from qrobot.models import AngularModel
-from qrobot.qunits import QUnit
+from qrobot.qunits import QUnit, SensorialUnit
 
 # Using pytest_check for this test to allow the whole test
 # to execute and stop the multithreading via unit.stop()
@@ -15,28 +15,26 @@ from qrobot.qunits import QUnit
 # and leaving the unit subprocesses open.
 
 
-def test_init_qunits() -> Tuple[QUnit, QUnit, QUnit]:
+@pytest.fixture
+def fixture_flush_redis() -> None:
+    """Flush redis before starting the test."""
+    qrobot.qunits.redis_utils.flush_redis()
+    check.equal(qrobot.qunits.redis_utils.redis_status(), {})
+
+
+@pytest.fixture
+def fixture_q_brain() -> Tuple[QUnit, QUnit, QUnit]:
+    """Initialize the qBrain."""
     # Layer 0
-    l0_unit0 = QUnit(
-        name="l0_unit0",
-        model=AngularModel(n=2, tau=5),
-        burst=ZeroBurst(),
-        Ts=0.05,
-        query=[0.1, 0.5],  # Query target initialized
-        # No input
-    )
+    l0_unit0 = SensorialUnit(name="l0_unit0", Ts=0.05)
     check.equal(
         dict(l0_unit0),
         {
             "name": "l0_unit0",
             "id": l0_unit0.id,
-            "model": "[model: AngularModel, n: 2, tau: 5]",
-            "burst": "<class 'qrobot.bursts.zeroburst.ZeroBurst'>",
-            "query": [0.1, 0.5],
             "Ts": 0.05,
         },
     )
-
     # Layer 1
     l1_unit0 = QUnit(
         name="l1_unit0",
@@ -45,6 +43,25 @@ def test_init_qunits() -> Tuple[QUnit, QUnit, QUnit]:
         Ts=0.2,
         in_qunits={0: l0_unit0.id},  # Will receive Input from l0_unit0, dim 0
     )
+    l1_unit1 = QUnit(
+        name="l1_unit1",
+        model=AngularModel(n=1, tau=5),
+        burst=ZeroBurst(),
+        Ts=0.2,
+        in_qunits={0: l0_unit0.id},  # Will receive input from l0_unit0, dim 1
+    )
+    # Return qBrain
+    q_brain = (l0_unit0, l1_unit0, l1_unit1)
+    return q_brain
+
+
+def test_init_qunits(
+    fixture_flush_redis,
+    fixture_q_brain: Tuple[QUnit, QUnit, QUnit],
+) -> None:
+    l0_unit0, l1_unit0, l1_unit1 = fixture_q_brain
+
+    # Check qunits initialization
     check.equal(
         dict(l1_unit0),
         {
@@ -55,13 +72,6 @@ def test_init_qunits() -> Tuple[QUnit, QUnit, QUnit]:
             "query": [0.0],
             "Ts": 0.2,
         },
-    )
-    l1_unit1 = QUnit(
-        name="l1_unit1",
-        model=AngularModel(n=1, tau=5),
-        burst=ZeroBurst(),
-        Ts=0.2,
-        in_qunits={0: l0_unit0.id},  # Will receive input from l0_unit0, dim 1
     )
     check.equal(
         dict(l1_unit1),
@@ -76,21 +86,17 @@ def test_init_qunits() -> Tuple[QUnit, QUnit, QUnit]:
     )
 
     # Check in_qunits
-    check.equal(l0_unit0.in_qunits, {0: None, 1: None})
     check.equal(l1_unit0.in_qunits, {0: l0_unit0.id})
     check.equal(l1_unit1.in_qunits, {0: l0_unit0.id})
 
-    # Set new query state for l0_unit0
-    check.equal(l0_unit0.query, [0.1, 0.5])
-    l0_unit0.query = [0.2, 0.4]
-    check.equal(l0_unit0.query, [0.2, 0.4])
+    # Set new query state for l1_unit0
+    check.equal(l1_unit0.query, [0])
+    l1_unit0.query = [0.2]
+    check.equal(l1_unit0.query, [0.2])
 
     # Check the input vector
-    check.equal(l0_unit0.input_vector, [0.0, 0.0])
     check.equal(l1_unit0.input_vector, [0.0])
     check.equal(l1_unit1.input_vector, [0.0])
-
-    return l0_unit0, l1_unit0, l1_unit1
 
 
 @pytest.mark.skip(
@@ -99,13 +105,12 @@ def test_init_qunits() -> Tuple[QUnit, QUnit, QUnit]:
     "at the mode.decode() step)"
 )
 @pytest.mark.redis
-def test_qunit():
-    # Flush redis before starting
-    qrobot.qunits.redis_utils.flush_redis()
-    check.equal(qrobot.qunits.redis_utils.redis_status(), {})
-
+def test_qunit(
+    fixture_flush_redis,
+    fixture_q_brain: Tuple[QUnit, QUnit, QUnit],
+):
     # Initialize qunits
-    l0_unit0, l1_unit0, l1_unit1 = test_init_qunits()
+    l0_unit0, l1_unit0, l1_unit1 = fixture_q_brain
 
     # Start unit tasks
     l0_unit0.start()
