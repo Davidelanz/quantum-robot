7f80a854e8a8abf4a376124f8f056b025b07bcc5
Davide Lanza <info@davidelanza.it>
2026-08-18 00:27:41 +0200

    test: add assertions for OneBurst and ZeroBurst classes

--- tests/core/bursts/test_oneburst.py
+++ tests/core/bursts/test_oneburst.py
@@ -3,4 +3,6 @@ from qrobot.bursts import OneBurst
 
 def test_burst():
     burst = OneBurst()
+    assert burst("0000") == 0.0
     assert burst("0010101010") == 4 / 10
+    assert burst("1111") == 1.0
--- tests/core/bursts/test_zeroburst.py
+++ tests/core/bursts/test_zeroburst.py
@@ -3,4 +3,6 @@ from qrobot.bursts import ZeroBurst
 
 def test_burst():
     burst = ZeroBurst()
+    assert burst("0000") == 1.0
     assert burst("0010101010") == 6 / 10
+    assert burst("1111") == 0.0
